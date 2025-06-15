930bc1f49f04eed950b0d927e83e82a7bc2b67d5
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2025-06-15 16:31:13 -0700

    changed velocity, nether compatible

--- src/main/java/systemerr/dynasmp/Timer.java
+++ src/main/java/systemerr/dynasmp/Timer.java
@@ -2,28 +2,27 @@ package systemerr.dynasmp;
 
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.util.Vector;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.function.Consumer;
 
-import static org.bukkit.Bukkit.getOnlinePlayers;
-import static org.bukkit.Bukkit.getServer;
-import static systemerr.dynasmp.Main.log;
+import static org.bukkit.Bukkit.*;
+import static org.bukkit.GameMode.SURVIVAL;
+import static systemerr.dynasmp.Utils.toTicks;
 
 public class Timer {
     public static int cooldown = 20;
-    public static int height = 40;
     public static int amount = 5;
-    
+
     public static int timer;
-    public static BossBar bossbar = Bukkit.createBossBar("", BarColor.RED, BarStyle.SEGMENTED_20);
+    public static BossBar bossbar = createBossBar("", BarColor.RED, BarStyle.SEGMENTED_20);
 
     public static void update() {
         for (int x = -2; x <= 2; x++) {
@@ -39,23 +38,32 @@ public class Timer {
             }
         }
         getServer().getWorlds().getFirst().getBlockAt(0, 319, 0).setType(Material.BEDROCK);
-        
+
         bossbar.setTitle("TNT Explodes In: " + timer);
         bossbar.setProgress((double) timer / cooldown);
         bossbar.setVisible(true);
-        
-        if (timer == 4) {
+
+        if (timer == 5) {
             getOnlinePlayers().forEach((Consumer<Player>) player -> {
-                if (player.getGameMode() == GameMode.SURVIVAL) {
+                if (player.getGameMode() == SURVIVAL) {
+                    int height;
+                    if (player.getWorld().getName().equals(getWorlds().get(1).getName())) {
+                        height = 120;
+                    } else if (player.getY() >= 318) {
+                        height = -65;
+                    } else {
+                        height = 318;
+                    }
                     for (int i = 0; i < amount; i++) {
-                        player.getWorld().getBlockAt((int) Math.round(player.getX()), (int) (Math.round(player.getY()) + height + 1), (int) Math.round(player.getZ())).setType(Material.REDSTONE_BLOCK);
-                        player.getWorld().getBlockAt((int) Math.round(player.getX()), (int) (Math.round(player.getY()) + height), (int) Math.round(player.getZ())).setType(Material.TNT);
-                        player.getWorld().getBlockAt((int) Math.round(player.getX()), (int) (Math.round(player.getY()) + height + 1), (int) Math.round(player.getZ())).setType(Material.AIR);
+                        TNTPrimed tnt = player.getWorld().spawn(new Location(player.getWorld(), Math.round(player.getX()), Math.round(player.getY()) + height, Math.round(player.getZ())), TNTPrimed.class);
+                        tnt.setFuseTicks(toTicks(5));
+                        tnt.setGlowing(true);
+                        tnt.setVelocity(new Vector(0, -20, 0));
                     }
                 }
             });
         }
-        
+
         if (timer <= 0) {
             timer = cooldown;
         }
